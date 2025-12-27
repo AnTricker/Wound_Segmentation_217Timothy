fd48863bb2cdb18b472705097b925bec48b1138e
張正璽 <timchang0217@gmail.com>
2025-12-27 19:14:17 +0800

    fix load checkpoint

--- utils/checkpoint.py
+++ utils/checkpoint.py
@@ -13,9 +13,12 @@ def save_checkpoint(path, model, optimizer, epoch, best_metrics=None):
 
 def load_checkpoint(path: str, model: torch.nn.Module, optimizer: torch.optim.Optimizer | None = None, map_location: str | torch.device = "cpu"):
     ckpt = torch.load(path, map_location=map_location)
+    
     model.load_state_dict(ckpt["model_state"])
-    if optimizer is not None and "optim_state" in ckpt:
-        optimizer.load_state_dict(ckpt["optim_state"])
+    if optimizer is not None and "optimizer" in ckpt:
+        optimizer.load_state_dict(ckpt["optimizer"])
+
     epoch = ckpt.get("epoch", 0)
     best_metric = ckpt.get("best_metric", None)
+
     return epoch, best_metric
